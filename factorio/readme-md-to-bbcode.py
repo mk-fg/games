5c1211d9c1d56b374811a8755608888fdd54ee72
Mike Kazantsev <mk.fraggod@gmail.com>
2018-10-02 15:05:18 +0500

    factorio.readme-md-to-bbcode: add -u/--urls-only option for current under-[spoiler] limitations

--- factorio/readme-md-to-bbcode.py
+++ factorio/readme-md-to-bbcode.py
@@ -11,10 +11,13 @@ def main(args=None):
 	parser.add_argument('-f', '--file',
 		metavar='file', default='README.md',
 		help='README file to process. Default: %(default)s')
+	parser.add_argument('-u', '--urls-only', action='store_true',
+		help='Only convert links from md to bbcode,'
+			' leaving rest as mostly-readable plaintext.')
 	opts = parser.parse_args(sys.argv[1:] if args is None else args)
 
 	text = pl.Path(opts.file).read_text()
-	text = re.sub(r'\[([^\]]+)\]\(([^)]+)\)', r'[url=\g<2>]\g<1>[/url]', text, re.DOTALL)
+	text = re.sub(r'\[([^\[\]]+)\]\(([^)]+)\)', r'[url=\g<2>]\g<1>[/url]', text, re.DOTALL)
 	lines = text.split('\n')
 
 	list_stack = list()
@@ -34,17 +37,22 @@ def main(args=None):
 		# Process lists
 		if line_is_item and (not list_stack or line_indent > list_stack[-1]):
 			list_stack.append(line_indent_item)
-			lines[n-1] += '\n{}[list]'.format(' '*line_indent)
+			if not opts.urls_only: lines[n-1] += '\n{}[list]'.format(' '*line_indent)
 		if list_stack:
 			if not line_is_empty and line_indent_item < list_stack[-1]:
-				lines[last_nonempty] += '\n{}[/list]'.format(' '*(list_stack[-1]-2))
+				if not opts.urls_only: lines[last_nonempty] += '\n{}[/list]'.format(' '*(list_stack[-1]-2))
 				list_stack.pop()
 				n -= 1
 				continue
-			if line_is_item: lines[n] = re.sub(r'^( *)- ', r'\g<1>[*]', lines[n])
+			if line_is_item:
+				if not opts.urls_only: lines[n] = re.sub(r'^( *)- ', r'\g<1>[*]', lines[n])
+				else:
+					m = re.search(r'^( +)- ', lines[n])
+					if m: lines[n] = m.group(1).replace('    ', '- ') + lines[n].lstrip()
 
 		# Process headers
-		lines[n] = re.sub(r'^( *)#+\s+(.*?)\s*$', r'\g<1>[h]\g<2>[/h]', lines[n])
+		if not opts.urls_only:
+			lines[n] = re.sub(r'^( *)#+\s+(.*?)\s*$', r'\g<1>[h]\g<2>[/h]', lines[n])
 
 		if not line_is_empty: last_nonempty = n
 	lines.pop()
